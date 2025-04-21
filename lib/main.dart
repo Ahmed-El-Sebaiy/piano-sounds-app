3f4d0d30cd34905a8eb01ad6991fa17b683c060c
Ahmed ElSebaiy <aelsebaiy1@gmail.com>
2025-04-21 15:50:09 +0200

    Initial commit - Piano Sounds Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:piano_sounds_app/views/tune_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: TuneView(),
+    );
+  }
+}
